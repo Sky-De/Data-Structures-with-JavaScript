d1dd640af940c0cd40ccbaac83b0c27a83d9200b
SkyDe <sky_de1991@outlook.com>
2023-03-15 23:12:02 +0330

    linkedList prepend added

--- DataStructures/4_linkedList/linkedList.js
+++ DataStructures/4_linkedList/linkedList.js
@@ -19,9 +19,23 @@ class LinkedList{
     getSize(){
         return this.size;
     }
+
+    prepend(value){
+        const node = new Node(value);
+        if(this.isEmpty()) this.head = node;
+        else {
+            node.next = this.head;
+            this.head = node;
+        }
+        this.size++;
+    }
 }
 
 
 const list = new LinkedList();
 console.log("is list empty ?", list.isEmpty());
 console.log("list size :", list.getSize());
+console.log("prepend 10 20 30 respectively");
+list.prepend(10);
+list.prepend(20);
+list.prepend(30);
