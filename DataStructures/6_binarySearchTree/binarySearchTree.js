1f0c8a23726f7eaf99a5e2d7356d1acd5dff9ae0
SkyDe <sky_de1991@outlook.com>
2023-04-07 15:46:21 +0430

    binarySearchTree search method added

--- DataStructures/6_binarySearchTree/binarySearchTree.js
+++ DataStructures/6_binarySearchTree/binarySearchTree.js
@@ -31,11 +31,26 @@ class BinarySearchTree {
             else this.insertNode(root.right, newNode);
         }
     }
-}
 
+    search(root ,value) {
+        if(!root) return false;
+        else{
+            if(value === root.value) return true;
+            else if(value < root.value) return this.search(root.left, value);
+            else return this.search(root.right, value);
+        }
+
+    }
+}
+// review--all
 const bst = new BinarySearchTree();
 console.log("is Tree empty ?", bst.isEmpty());
 console.log("inserting 10, 5, 15 to the tree using inerst method--------------------");
 bst.insert(10);
 bst.insert(5);
 bst.insert(15);
+console.log("search for 10,5,15 and 30 using search method------------");
+console.log("search for 10", bst.search(bst.root, 10));
+console.log("search for 5", bst.search(bst.root, 5));
+console.log("search for 15", bst.search(bst.root, 15));
+console.log("search for 30", bst.search(bst.root, 30));
